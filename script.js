3156447ede448ef302594f7c2512db691044cd3a
MohammedAref11 <245248570+MohammedAref11@users.noreply.github.com>
2026-02-06 18:06:21 +0400

    Finished styling for the page

---
+++ script.js
@@ -0,0 +1,8 @@
+let addBtn = document.getElementById("addition-btn"); 
+let subBtn = document.getElementById("subtraction-btn"); 
+let mulBtn = document.getElementById("mutiplication-btn"); 
+let diviBtn = document.getElementById("division-btn"); 
+
+let num1 = document.getElementById("num1-el"); 
+let num2 = document.getElementById("num2-el"); 
+
